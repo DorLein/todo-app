ea5111d62c1f0bad64ad17daeafcf5a18cbd197a
DorLein <abdellah.salahi@gmail.com>
2023-08-07 12:35:37 +0100

    2nd

---
+++ src/components/todoitem.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./todoitem.module.css";
+
+function Todoitem() {
+  return (
+    <div className={styles.todos}>
+     <h1>Todo App</h1>
+      <div className={styles.Header}>
+        <div className={styles.input_container}>
+          <div className={styles.circle}></div>
+          <form>  
+            <input type="text" placeholder="Create a new todo ..." /><br></br>
+            <input type="checkbox"></input>
+            <input type ="text" placeholder="Todo created" />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Todoitem;
